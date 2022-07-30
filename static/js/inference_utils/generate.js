aba2bedc3ec25ac6b82f31c106c8c480047e4ef0
Kopper <63787479+kopper-xdu@users.noreply.github.com>
2022-07-30 09:10:55 +0800

    Update generate.js

--- static/js/inference_utils/generate.js
+++ static/js/inference_utils/generate.js
@@ -6,12 +6,11 @@ const session = ort.InferenceSession
             {executionProviders: ['wasm'], graphOptimizationLevel: 'all'});
 
 export async function inference(imgPath1, imgPath2) {
-    // 1. Convert image to tensor
     const imageTensor1 = await getImageTensorFromBuffer(imgPath1);
     const imageTensor2 = await getImageTensorFromBuffer(imgPath2);
 
-    // 2. Run model
+    
     const [result, inferenceTime] = await runAdaINModel(session, imageTensor1, imageTensor2);
-    // 3. Return predictions and the amount of time it took to inference_utils.
+   
     return [result, inferenceTime];
 }
